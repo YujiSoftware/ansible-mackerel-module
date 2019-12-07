e64d4a007ce9c3797564ac0bdaa30b2d03b118e4
YujiSoftware <yuji.software+github@gmail.com>
2019-12-08 01:33:53 +0900

    Python2 support

--- library/mackerel_host.py
+++ library/mackerel_host.py
@@ -3,17 +3,23 @@
 
 import json
 import re
-import urllib2
+
+import ansible.module_utils.six.moves.urllib.error as urllib_error
 
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
 
-def get_host(host, api_key):
-    url = "https://api.mackerelio.com/api/v0/hosts/%s" % host
-    req = urllib2.Request(url)
-    req.add_header("X-Api-Key", api_key)
+def fetch_mackerel(module, api_key, path, data={}, method="GET"):
+    url = "https://api.mackerelio.com/api/v0/%s" % path
+    headers = {
+        'Content-type': 'application/json',
+        "X-Api-Key": api_key,
+    }
+    resp, info = fetch_url(module, url, json.dumps(data), headers, method)
+    if info["status"] != 200:
+        raise urllib_error.HTTPError(url, info["status"], info["body"], headers, None)
 
-    res = urllib2.urlopen(req)
-    body = res.read()
+    body = resp.read()
     
     return json.loads(body)
 
@@ -23,13 +29,37 @@ def main():
             host=dict(type='str'),
             status=dict(type='str', default=None, choices=['standby', 'working', 'maintenance', 'poweroff']),
             role_fullnames=dict(type='list', default=[]),
-        )
+            apikey=dict(type='str'),
+            conf=dict(type='str', default='/etc/mackerel-agent/mackerel-agent.conf'),
+            root=dict(type='str', default='/var/lib/mackerel-agent'),
+        ),
+        supports_check_mode=True,
     )
     args = module.params
 
     host = args.get('host')
     status = args.get('status')
     role_fullnames = args.get('role_fullnames')
+    apikey = args.get('apikey')
+    conf = args.get('conf')
+    root = args.get('root')
+
+    if host is None:
+        with open("%s/id" % root) as file:
+            host = file.read()
+
+    if apikey is None:
+        # TODO: Reading TOML in a better way
+        apikey_re = re.compile(r'^\s*apikey\s*=\s*"(\w+)"$')
+        with open(conf) as file:
+            for line in file:
+                match = apikey_re.match(line)
+                if match:
+                    apikey = match.group(1)
+                    break
+        if apikey is None:
+            msg = "Could not read mackerel api key from '%s'" % conf
+            module.fail_json(msg=msg)
 
     roles = {}
     for fullname in role_fullnames:
@@ -39,48 +69,28 @@ def main():
         else:
             roles[key] = [value]
 
-    if host is None:
-        with open("/var/lib/mackerel-agent/id") as file:
-            host = file.read()
+    data = fetch_mackerel(module, apikey, "hosts/%s" % host)
+    changed = False
 
-    api_key_re = re.compile(r'apikey = "(\w+)"')
-    with open("/etc/mackerel-agent/mackerel-agent.conf") as file:
-        for line in file:
-            match = api_key_re.match(line)
-            if match:
-                api_key = match.group(1)
-                break
-   
-    try:
-        data = get_host(host, api_key)
-        changed = False
+    if status is not None and data["host"]["status"] != status:
+        url = "hosts/%s/status" % host
+        values = {'status' : status }
+        if not module.check_mode:
+            fetch_mackerel(module, apikey, url, values, "POST")
 
-        if status is not None and data["host"]["status"] != status:
-            url = "https://api.mackerelio.com/api/v0/hosts/%s/status" % host
-            values = {'status' : status }
-            req = urllib2.Request(url, json.dumps(values))
-            req.add_header("X-Api-Key", api_key)
-            req.add_header("Content-Type", "application/json")
-            urllib2.urlopen(req)
+        data["host"]["status"] = status
+        changed = True
 
-            data["host"]["status"] = status
-            changed = True
-            
-        if len(role_fullnames) > 0 and data["host"]["roles"] != roles:
-            url = "https://api.mackerelio.com/api/v0/hosts/%s/role-fullnames" % host
-            values = {'roleFullnames' : role_fullnames }
-            req = urllib2.Request(url, json.dumps(values))
-            req.get_method = lambda: 'PUT'
-            req.add_header("X-Api-Key", api_key)
-            req.add_header("Content-Type", "application/json")
-            urllib2.urlopen(req)
+    if len(role_fullnames) > 0 and data["host"]["roles"] != roles:
+        url = "hosts/%s/role-fullnames" % host
+        values = {'roleFullnames' : role_fullnames }
+        if not module.check_mode:
+            fetch_mackerel(module, apikey, url, values, "PUT")
 
-            data["host"]["roles"] = roles
-            changed = True
-            
-        module.exit_json(data=data, changed=changed)
-    except urllib2.HTTPError as e:
-        module.fail_json(msg="%s: %s (%s)" % (e.code, json.loads(e.read())["error"], url))
+        data["host"]["roles"] = roles
+        changed = True
+
+    module.exit_json(host=data["host"], changed=changed)
 
 if __name__ == '__main__':
     main()
